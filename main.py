b4225b259cb52ccd017de378d6444029d80c4d6a
Salvo888 <smholdingsgroup888@gmail.com>
2025-06-27 21:32:05 +1000

    change

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+API_KEY = "9359e7f4b92aacc1aaf4914925a53a46"
 @app.get("/airplanemodel")
 def get_model(airplanes: str):
-    url = f"https://api.aviationstack.com/v1/airplanes?access_key=9359e7f4b92aacc1aaf4914925a53a46&{airplanes}"
+    url = f"https://api.aviationstack.com/v1/{airplanes}?access_key={API_KEY}"
     response = requests.get(url, timeout=5)
-    data = response.json()
-    history = data.get("data")
+    data_1 = response.json()
+    history = data_1.get("data")
     return {"airplanes": airplanes, 'id': history.get("id"), 
                 'construction_number': history.get('construction_number'), 
                 'engines_type': history.get('engines_type'),
